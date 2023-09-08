38d2d0893888f2438fd185d5636bb0f6baa0e6a2
Igor Machado <igor.machado@gmail.com>
2023-09-08 20:37:59 +0000

    feat: working fine on linux

--- CMakeProject2/script-deps.sh
+++ CMakeProject2/script-deps.sh
@@ -7,26 +7,43 @@ conan profile detect
 
 VCPKG_DIR=./deps/vcpkg
 VCPKG_TRIPLET=x64-windows
+VCPKG_BOOTSTRAP=bootstrap-vcpkg.bat
 
 COUNT_LINUX=`conan profile show | grep Linux | wc -l`
 if [[ "$COUNT_LINUX" -ne 0 ]]; then
     VCPKG_TRIPLET=x64-linux
+    VCPKG_BOOTSTRAP=bootstrap-vcpkg.sh
 fi
 
 echo "VCPKG_TRIPLET=$VCPKG_TRIPLET"
 
 mkdir -p deps
 git submodule add https://github.com/microsoft/vcpkg $VCPKG_DIR
-$VCPKG_DIR/bootstrap-vcpkg.sh
 
-# mpir for windows?
+(cd $VCPKG_DIR && git checkout master && git pull)
+$VCPKG_DIR/$VCPKG_BOOTSTRAP
+
+# update DOES NOT WORK IN MANIFEST MODE!
+# $VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR update
+
 $VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR search gmp
+$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR search mpir
 
-(cd $VCPKG_DIR && git pull)
+$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR integrate install
 
-$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR update
+$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR install
 
-$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR install gmp # --triplet=$VCPKG_TRIPLET
+#$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR remove gmp # --triplet=$VCPKG_TRIPLET
+
+#$VCPKG_DIR/vcpkg --vcpkg-root $VCPKG_DIR install gmp --triplet=$VCPKG_TRIPLET
+
+# vcpkg integrate install 
+# https://devblogs.microsoft.com/cppblog/vcpkg-september-2022-release-is-now-available-celebrating-6-years-with-over-2000-libraries/
+# https://learn.microsoft.com/en-us/vcpkg/examples/manifest-mode-cmake
+
+###vcpkg install mpir --triplet=x64-windows-static
+#
+#
 # CMake Error at scripts/cmake/vcpkg_configure_make.cmake:625 (message):
 #  gmp requires autoconf from the system package manager (example: "sudo
 #  apt-get install autoconf")
