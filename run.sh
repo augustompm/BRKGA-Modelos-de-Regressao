a373cb8bee85deb1f56860477984c761a5dda5f6
Augusto Mendonca <augusto01@gmail.com>
2024-12-30 01:22:12 +0000

    Primeiro commit na branch gecco

--- run.sh
+++ run.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 # New Runner @aug
-seeds="100001 100002 100003 100004 100005 100006 100007 100008 100009 100010 100011 100012 100013 100014 100015 100016 100017 100018 100019 100020 100021 100022 100023 100024 100025 100026 100027 100028 100029 100030"
-instances=(
-    "Produto_interno_100.in"
-    "Distancia_euclidiana_100.in"
-    "Densidade_probabi_100.in"
-) 
 
-results_dir="results"
+# seeds 
+seeds="100001"
+
+# instâncias 
+instances=(
+"bose-einstein_100_III.4.32.in"
+"Campo_eletrico_100_I.12.4.in"
+"Densidade_de_probabilidade_100_I.6.20a.in"
+"Distancia_Euclidiana_100_I.8.14.in"
+"Energia_cinetica_total_100_I.13.4.in"
+"Energia_potencial_Elastica_mola_100_I.14.4.in"
+"Energia_potencial_Gravitacional_100_I.14.3.in"
+"Energia_potencial_gravitacional_com_constante_gravitacional_100_I.13.12.in"
+"Energia_total_com_frequencia_100_l.24.6.in"
+"Força_Atrito_100_I.12.1.in"
+"Força_eletrica_100_I.12.5.in"
+"Força_eletrica_entre_duas_cargas_100_I.12.2.in"
+"Forca_gravitacional_100_I.9.18.in"
+"Lentes_delgadas_100_I.27.6.in"
+"Massa_relativistica_100_I.10.7.in"
+"Ondas_eletromagneticas_100_II.24.17.in"
+"Periodo_orbital_100_Goldstein 3.74 (Kepler).in"
+"Posição_centro_de_massa_100_I.18.4.in"
+"Produto_interno_100_I.11.19.in"
+"transformação_Lorentz_100_I.15.3x.in"
+"transformação_Lorentz_tempo_100_I.15.3t.in"
+)
+
+# Diretório de resultados
+results_dir="GECCO_results"
 mkdir -p "$results_dir"
 
-# Instâncias
+# Executa para cada instância
 for instance in "${instances[@]}"; do
     echo "Executando para a instância $instance..."
-    
-    # Seeds $seeds
+
     i=01
     for s in $seeds; do
-        output_file="${results_dir}/${instance%.in}_${s}_$i.txt"
-        echo "Executando ./bazel-bin/app_demo $s running_instances/$instance"
         
-        # Verifica
+        output_file="${results_dir}/${instance%.in}_${s}_$i.txt"
+        echo "Executando ./bazel-bin/app_demo $s instances_GECCO/$instance 125 25 15 85 100 50 5 5"
+        
         if [ -f "./bazel-bin/app_demo" ]; then
-            ./bazel-bin/app_demo "$s" "running_instances/$instance" >> "$output_file"
+            ./bazel-bin/app_demo "$s" "instances_GECCO/$instance" 125 25 15 85 100 50 5 5 >> "$output_file"
             echo "Resultado salvo em $output_file"
         else
             echo "Erro: o binário app_demo não foi encontrado."
